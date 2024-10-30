af418f25e0c2b2a9cdde984f03169a539a9bcadb
NPetkov96 <126357169+NPetkov96@users.noreply.github.com>
2024-10-31 01:45:49 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyHomeNurse.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
