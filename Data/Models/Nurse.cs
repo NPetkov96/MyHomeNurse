592270f6c4ad124821f605c75b5cbead07b501d5
NPetkov96 <126357169+NPetkov96@users.noreply.github.com>
2024-10-31 01:50:41 +0200

    Added first model: Nurse

---
+++ Data/Models/Nurse.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyHomeNurseCommon.Constants;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyHomeNurse.Data.Models
+{
+    public class Nurse
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(NurseConstants.MaxLenghtName)]
+        public string FirstName { get; set; } = null!;
+
+        public string LastName { get; set; } = null!;
+
+        [Required]
+        [Comment("Min years of experience of the nurse")]
+        public int YearExperience { get; set; }
+
+        public int Age { get; set; }
+
+    }
+}
